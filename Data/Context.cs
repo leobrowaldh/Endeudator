30050a8e0a63b675fe0fa207bd1d542e6a222dd7
leobrowaldh <leobrowaldh@gmail.com>
2024-01-10 13:28:59 +0100

    connection string back to how it was originally

--- Data/Context.cs
+++ Data/Context.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Endeudator.Models;
+using Microsoft.EntityFrameworkCore.Design;
 
 namespace Endeudator.Data
 {
@@ -9,21 +10,16 @@ namespace Endeudator.Data
         public DbSet<Movement> Movements { get; set; }
         public DbSet<Interest> Interests { get; set; }
         public DbSet<CurrentRate> CurrentRates { get; set; }
-        public string ConnectionString { get; private set; }
 
-        public Context()
-        {
-            //The connection string should be located in [App directory]/Config/ConnectionString.txt
-            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
-            string filePath = Path.Combine(baseDirectory, "Config", "ConnectionString.txt");
-            ConnectionString = File.ReadAllText(filePath).Trim();
-        }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-                optionsBuilder.UseSqlServer(ConnectionString);
-            }
+            optionsBuilder.UseSqlServer(@"
+                    Server=localhost; 
+                    Database=Endeudator; 
+                    Trusted_Connection=True; 
+                    Trust Server Certificate=Yes; 
+                    User Id=Endeudator; 
+                    password=Endeudator");
         }
     }
 }
